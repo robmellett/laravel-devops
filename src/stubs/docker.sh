9a15639a4124d021f00b39af4f2489722671c8ba
Rob <dev@robmellett.com>
2020-03-14 11:57:43 +1000

    chmod +x docker.sh

--- src/stubs/docker.sh
+++ src/stubs/docker.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-# Find container names with `docker container ls`
-
-echo ">> Database IP:"
-echo $(docker inspect docker-mysql | grep IPAddress)
-
 # SSH into Web App
 docker exec -it --user ubuntu acme-app bash
